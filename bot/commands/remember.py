9cb2e919915c99c09d2e7fb492bf811a4c583df7
Michael Henry <michael.henry@sure.technology>
2017-11-26 00:34:56 -0500

    Added a list and delete command.

--- bot/commands/remember.py
+++ bot/commands/remember.py
@@ -1,5 +1,6 @@
 from bot.command_map import command_map
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import botocore
 import os
 import logging
@@ -93,6 +94,44 @@ def put_item_dynamo(query={}, user=""):
         return answers
 
 
+def get_all_prefs(user=""):
+    table = use_table()
+    answer = ""
+    response = table.query(KeyConditionExpression=Key('name').eq(user))
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        try:
+            answer += "\n".join("*{}*: {}".format(x['command'], x['preference']) for x in response['Items'])
+        except botocore.exceptions.ClientError as scna_e:
+            logger.error("Couldn't find data during table scan: {}".format(scna_e))
+            pass
+        except KeyError as scan_key_error:
+            logger.debug('Unable to find data in Dynamo.'.format(scan_key_error))
+            pass
+    return answer
+
+
+def delete_preference(to_remove={}, user=""):
+    answer = ""
+    removed = []
+    try:
+        table = use_table()
+        for command, preference in to_remove.items():
+            response = table.delete_item(Key={'name': user, 'command': command})
+            removed.append(command)
+    except botocore.exceptions.ClientError as delete_client_error:
+        if delete_client_error.response['Error']['Code'] == "ConditionalCheckFailedException":
+            logger.error("Client Error when delete_preference("
+                         "to_remove= {}, user={}) was run.\n{}".format(to_remove,
+                                                                       user,
+                                                                       delete_client_error.response['Error']['Message']))
+        else:
+            logger.error("Something else went wrong while deleting "
+                         "preference from Dynamo.: {}".format(delete_client_error.response))
+    else:
+        answer += "I removed {}. Use `!remember list` to see what is left. ".format(", ".join(removed))
+    return answer
+
+
 def read_from_dynamodb(query=[], user=""):
     user_input = dict(i.split(':') for i in query)
     table = use_table()
@@ -115,7 +154,7 @@ def read_from_dynamodb(query=[], user=""):
                                                         response['Item']['preference']))
                 commands[response['Item']['command']] = response['Item']['preference']
             except botocore.exceptions.ClientError as read_e:
-                logger.error("Couldnt find {} for {}: {}".format(command, user, read_e))
+                logger.error("Couldn't find {} for {}: {}".format(command, user, read_e))
                 continue
             except KeyError as key_error:
                 logger.debug('Unable to find command {} for {} in Dynamo.'.format(command, user))
@@ -135,14 +174,26 @@ def remember(query=[], user=""):
     Have me remember command preferences for you.
     --------------------------------------------------------
     *Usage:*
-    `!remember music:google weather:20170`
+    `!remember music:google weather:20170` - Add preference.
+    `!remember list` - List your current preferences.
+    `!remember google: delete`
     Each `command:preference` set *MUST* be set in the format indicated.
+    I can set preferences for: {}
     --------------------------------------------------------
-    '''
+    '''.format(", ".join(sorted(remember_options)))
     logger.debug("Trying to update user preferences.\nUser: {}, Query {}".format(user, query))
-    user_input = valid_option(query)
-    if query and user_input:
-        # Update Entries and return entries.
+    if "list" in query:
+        prefs = get_all_prefs(user)
+        if prefs:
+            response = "*Here are your preferences:*\n{}".format(prefs)
+        else:
+            response = "You don't have any preferences. Use `!help remember` for help creating some"
+    elif 'delete' in query:
+        query = [x for x in query if x.lower() != 'delete']
+        user_input = valid_option(query)
+        response = delete_preference(to_remove=user_input, user=user)
+    elif query and valid_option(query):
+        user_input = valid_option(query)
         response = "I'm awesome! I updated these:\n"
         try:
             updated_items = put_item_dynamo(user_input, user)
